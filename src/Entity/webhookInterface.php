85d9e75cad3e9fe2e184e2863de58aa3b56d6aca
Insasse <dirk.schmidt@erdfisch.de>
2016-06-28 11:10:49 +0200

    master - add interface ghost methods

--- src/Entity/webhookInterface.php
+++ src/Entity/webhookInterface.php
@@ -10,4 +10,58 @@ use Drupal\Core\Config\Entity\ConfigEntityInterface;
 interface webhookInterface extends ConfigEntityInterface {
 
   // Add get/set methods for your configuration properties here.
+  /**
+   * @return string
+   */
+  public function getId();
+
+  /**
+   * @return string
+   */
+  public function getLabel();
+
+  /**
+   * @return string
+   */
+  public function getPayloadUrl();
+
+  /**
+   * @return string
+   */
+  public function getEvents();
+
+  /**
+   * @return string
+   */
+  public function getContentType();
+
+  /**
+   * @return boolean
+   */
+  public function isStatus();
+
+  /**
+   * @return int
+   */
+  public function getLastUsage();
+
+  /**
+   * @return boolean
+   */
+  public function isResult();
+
+  /**
+   * @return string
+   */
+  public function getEntityType();
+
+  /**
+   * @return string
+   */
+  public function getEntityId();
+
+  /**
+   * @return string
+   */
+  public function getSecret();
 }
