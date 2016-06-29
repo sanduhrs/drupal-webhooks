e27d339b465d154012ed32ce6e7be22f021393ce
Insasse <dirk.schmidt@erdfisch.de>
2016-06-29 09:23:11 +0200

    master - back to default listbuilder

--- src/webhookListBuilder.php
+++ src/webhookListBuilder.php
@@ -17,15 +17,6 @@ class webhookListBuilder extends ConfigEntityListBuilder {
   public function buildHeader() {
     $header['label'] = $this->t('Webhook');
     $header['id'] = $this->t('Machine name');
-    $header['status'] = $this->t('Status');
-    $header['payload_url'] = $this->t('Payload URL');
-    $header['last_usage'] = $this->t('Last Usage');
-    $header['events'] = $this->t('Events');
-    $header['result'] = $this->t('Result');
-    $header['ref_entity_type'] = $this->t('Reference Entity Type');
-    $header['entity_id'] = $this->t('Entity ID');
-    $header['secret'] = $this->t('Secret');
-    $header['content_type'] = $this->t('Content Type');
     return $header + parent::buildHeader();
   }
 
@@ -33,18 +24,9 @@ class webhookListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    $webhook = new webhook();
+
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
-    $row['status'] = $webhook->isStatus();
-    $row['payload_url'] = $webhook->getPayloadUrl();
-    $row['last_usage'] = $webhook->getLastUsage();
-    $row['events'] = $webhook->getEvents();
-    $row['result'] = $webhook->isResult();
-    $row['ref_entity_type'] = $webhook->getrefEntityType();
-    $row['entity_id'] = $webhook->getEntityId();
-    $row['secret'] = $webhook->getSecret();
-    $row['content_type'] = $webhook->getContentType();
     // You probably want a few more properties here...
     return $row + parent::buildRow($entity);
   }
