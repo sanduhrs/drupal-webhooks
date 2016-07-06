efe9b14657757c2074e3be16174cc19f89d025c7
Stefan Auditor <stefan.auditor@erdfisch.de>
2016-07-06 17:08:08 +0200

    Update service class, implement post and encoding

--- src/WebhookService.php
+++ src/WebhookService.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\webhooks;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\webhooks\Entity\Webhook;
 use Drupal\webhooks\Entity\WebhookInterface;
+use GuzzleHttp\Client;
 
 /**
  * Class WebhookService.
@@ -18,18 +20,72 @@ class WebhookService extends Webhook implements WebhookServiceInterface {
   protected $client;
 
   /**
-   * Constructor.
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
    */
-  public function __construct() {
-    $this->client = \Drupal::httpClient();
+  protected $loggerFactory;
+
+
+
+  /**
+   * WebhookService constructor.
+   *
+   * @param \GuzzleHttp\Client $client
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   */
+  public function __construct(
+      Client $client,
+      LoggerChannelFactoryInterface $logger_factory
+  ) {
+    $this->client = $client;
+    $this->loggerFactory = $logger_factory;
   }
 
   /**
-   * @param \Drupal\webhooks\Webhook $webhook
+   * @param \Drupal\webhooks\Entity\Webhook $webhook
    * @param \Drupal\webhooks\Payload $payload
    */
-  public function send(Payload $payload) {
-    $this->client->post($this->getPayloadUrl(),$payload->getPayload());
+  public function send(Webhook $webhook, Payload $payload) {
+    $string = self::encode(
+      $payload->getPayload(),
+      $webhook->getContentType()
+    );
+    try {
+      $this->client->post(
+        $webhook->getPayloadUrl(),
+        ['body' => $string]
+      );
+    } catch (\Exception $e) {
+      // TODO: Fixme
+      $this->loggerFactory->get('webhooks')->error($e->getMessage());
+    }
+  }
+
+  /**
+   * @param $data
+   * @param $content_type
+   * @return bool|string|\Symfony\Component\Serializer\Encoder\scalar
+   */
+  public static function encode($data, $content_type) {
+    /** @var \Drupal\serialization\Encoder\JsonEncoder $encoder */
+    $encoder = \Drupal::service('serializer.encoder.' . $content_type);
+    if (!empty($encoder) && $encoder->supportsEncoding($content_type)) {
+      return $encoder->encode($data, $content_type);
+    }
+    return '';
+  }
+
+  /**
+   * @param $data
+   * @param $content_type
+   * @return bool|string|\Symfony\Component\Serializer\Encoder\scalar
+   */
+  public static function decode($data, $content_type) {
+    /** @var \Drupal\serialization\Encoder\JsonEncoder $encoder */
+    $encoder = \Drupal::service('serializer.encoder.' . $content_type);
+    if (!empty($encoder) && $encoder->supportsDecoding($content_type)) {
+      return $encoder->decode($data);
+    }
+    return false;
   }
 
   public function receive() {
@@ -43,9 +99,9 @@ class WebhookService extends Webhook implements WebhookServiceInterface {
       case 'x-www-form-urlencoded':
           urldecode();
         break;
-
       default :
-        \Drupal::logger('webhooks')->error('None supported CONTENT_TYPE was recognized.');
+        $this->loggerFactory->get('webhooks')
+          ->error('Content-type not supported.');
         break;
     }
   }
