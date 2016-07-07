46c589c9c589ba4a5634665b2e0714be13124017
Nikolas Kopp <nikolas.kopp@erdfisch.de>
2016-07-07 16:13:44 +0200

    add indicator if webhook is active

--- src/WebhookConfigListBuilder.php
+++ src/WebhookConfigListBuilder.php
@@ -14,6 +14,7 @@ class WebhookConfigListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildHeader() {
+    $header['active'] = $this->t('Status');
     $header['label'] = $this->t('Webhook');
     $header['id'] = $this->t('Machine name');
     return $header + parent::buildHeader();
@@ -23,7 +24,7 @@ class WebhookConfigListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-
+    $row['active'] = $entity->isActive() ? $this->t('Active') : $this->t('Inactive');
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
     return $row + parent::buildRow($entity);
