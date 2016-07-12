bc254e3d4ff95dcf2dbdc3a032210060dccabe8b
Stefan Auditor <stefan.auditor@erdfisch.de>
2016-07-12 11:07:05 +0200

    Switch service loading to dependency injection

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -4,8 +4,11 @@ namespace Drupal\webhooks\Controller;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\webhooks\WebhooksService;
 use GuzzleHttp\Psr7\Response;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class Webhook.
@@ -14,6 +17,36 @@ use GuzzleHttp\Psr7\Response;
  */
 class WebhookController extends ControllerBase {
 
+  protected $webhooksService;
+
+  protected $entityTypeManager;
+
+  /**
+   * WebhookController constructor.
+   *
+   * @param \Drupal\webhooks\WebhooksService $webhooks_service
+   *   The Webhooks service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(
+      WebhooksService $webhooks_service,
+      EntityTypeManagerInterface $entity_type_manager
+  ) {
+    $this->webhooksService = $webhooks_service;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('webhooks.service'),
+      $container->get('entity_type.manager')
+    );
+  }
+
   /**
    * Webhooks receiver.
    *
@@ -21,9 +54,7 @@ class WebhookController extends ControllerBase {
    *   Return 200 OK.
    */
   public function receive() {
-    /** @var \Drupal\webhooks\WebhooksService $webhooksService */
-    $webhooksService = \Drupal::service('webhooks.service');
-    $webhooksService->receive();
+    $this->webhooksService->receive();
     return new Response(200, [], 'OK');
   }
 
@@ -50,7 +81,8 @@ class WebhookController extends ControllerBase {
    *   A redirect response object that may be returned by the controller.
    */
   public function toggleActive($id) {
-    $webhooks_storage = \Drupal::entityTypeManager()->getStorage('webhook_config');
+    $webhooks_storage = $this->entityTypeManager->getStorage('webhook_config');
+    /** @var \Drupal\webhooks\Entity\WebhookConfig $webhook_config */
     $webhook_config = $webhooks_storage->load($id);
     $webhook_config->setStatus(!$webhook_config->status());
     $webhook_config->save();
